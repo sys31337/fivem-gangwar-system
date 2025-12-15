f32390f36a82bbb59e1b44c0a7f8a07d3040abe8
Meb. Mohamed Rida <paytf@outlook.com>
2025-12-15 21:07:51 +0100

    Fix client NUI callbacks to use correct server event names

--- client/nui.lua
+++ client/nui.lua
@@ -1,40 +1,52 @@
--- NUI callbacks and communication
+-- NUI Callbacks - CLIENT SIDE ONLY
+-- RegisterNuiCallback is for client scripts receiving messages from React frontend
 
--- NUI to Lua communication
+-- NUI Callback: NUI is ready
 RegisterNuiCallback('nuiReady', function(data, cb)
-    print('^2[NUI] Frontend ready^7')
+    print('^2[Gangwar NUI] Frontend ready^7')
     cb({success = true})
 end)
 
+-- NUI Callback: Close UI
 RegisterNuiCallback('closeUI', function(data, cb)
     SetNuiFocus(false, false)
     cb({success = true})
 end)
 
+-- NUI Callback: Start war (from React frontend)
 RegisterNuiCallback('startWar', function(data, cb)
-    TriggerServerEvent('gangwar:startWar', data.attacker, data.defender)
+    print('[Gangwar] Starting war: ' .. data.attacker .. ' vs ' .. data.defender)
+    TriggerServerEvent('gangwar:server:startWar', data.attacker, data.defender)
     cb({success = true})
 end)
 
+-- NUI Callback: End war (from React frontend)
 RegisterNuiCallback('endWar', function(data, cb)
-    TriggerServerEvent('gangwar:endWar')
+    print('[Gangwar] Ending war')
+    TriggerServerEvent('gangwar:server:endWar')
     cb({success = true})
 end)
 
+-- NUI Callback: Capture territory (from React frontend)
 RegisterNuiCallback('captureTerritory', function(data, cb)
-    TriggerServerEvent('gangwar:captureTerritory', data.gangId, data.territoryId)
+    print('[Gangwar] Capturing territory ' .. data.territoryId .. ' for gang ' .. data.gangId)
+    TriggerServerEvent('gangwar:server:captureTerritory', data.gangId, data.territoryId)
     cb({success = true})
 end)
 
+-- NUI Callback: Get stats (from React frontend)
 RegisterNuiCallback('getStats', function(data, cb)
-    TriggerServerEvent('gangwar:getStats')
+    print('[Gangwar] Getting stats')
+    TriggerServerEvent('gangwar:server:getStats')
     cb({success = true})
 end)
 
--- Lua to NUI communication
+-- Send data to NUI
 function SendToNUI(type, data)
     SendNuiMessage(json.encode({
         type = type,
         data = data,
     }))
 end
+
+print('^2[Gangwar NUI] Callbacks registered successfully^7')
